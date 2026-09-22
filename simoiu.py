18731c3749521c4f575b1fae6c3e5a1c932471a7
Peter Steiglechner <steiglechner@csh.ac.at>
2026-09-22 14:34:51 +0200

    updated simoiu

--- simoiu.py
+++ simoiu.py
@@ -64,23 +64,34 @@ def get_chain(task, cond):
     return chain 
 
 
-selected_tasks = [10140+l for l in range(20)][-7:-2] # age questions
-selected_tasks = [10740+l for l in range(20)]
-fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
+# selected_tasks = [10140+l for l in range(20)][-7:-2] # age questions
+selected_tasks = [l for l in range(10740, 10760) if not l == 10752]
+fig, axs = plt.subplots(2,2, sharex=True, sharey=True, figsize=(18/2.54, 14/2.54))
+xis = []
 conds = ['Control', 'Consensus', 'Most recent','Most confident'] #answers.experimental_condition.unique()
 for ax, cond in zip(axs.flatten(), conds):
     n_peepsPerChainCond = n_peepsPerChain.loc[n_peepsPerChain["experimental_condition"]==cond]
-    cols = plt.get_cmap("tab10")
+    cols = plt.get_cmap("tab20b")
+    xis_med = []
+    xis_mean = []
     for ni, task in enumerate(selected_tasks):
         c = cols(ni)
         # taskname = tasks.loc[tasks.task_id==task]["prompt"].str.replace("How old is ", "").str.replace("?", "").iloc[0]
-        taskname = tasks.loc[tasks.task_id==task]["path_to_media"].str.replace("prediction_movie_rating/", "").str.replace(".mp4", "").iloc[0]
+        taskname = tasks.loc[tasks.task_id==task]["path_to_media"].str.replace("prediction_movie_rating/", "").str.replace(".mp4", "").str.replace("_", " ").iloc[0]
+
+        xi_mean = (abs(chain.answer.astype(float).mean()- chain.correct_answer.unique().astype(float))/chain.std_answer.unique())[0]
+        xis_mean.append(xi_mean)
+        xi_median = (abs(chain.answer.astype(float).median()- chain.correct_answer.unique().astype(float))/chain.std_answer.unique())[0]
+        xis_med.append(xi_median)
+        xis.append([cond, taskname, xi_mean, xi_median])
+
+        # taskname += fr" $\xi={xi:.1f}$" 
         chain = get_chain(task, cond)
-        chain["relanswer"].plot(ax=ax, label="_", color=c, alpha=0.2, lw=0, marker="o", markersize=2)
-        chain["rollMean"].plot(ax=ax, color=c, label=taskname)
-        chain["rollMedian"].plot(ax=ax, color=c, ls="--", label="_")
+        chain["relanswer"].plot(ax=ax, label="_", color=c, alpha=0.1, lw=0, marker="o", markersize=1)
+        chain["rollMean"].plot(ax=ax, color=c, label=taskname, alpha=0.5)
+        chain["rollMedian"].plot(ax=ax, color=c, ls="--", label="_", alpha=0.5)
         # --- right: histogram of language counts ---
-        y_grid = np.linspace(min(chain["relanswer"])*1.2, max(chain["relanswer"])*1.2, 100)
+        y_grid = np.linspace(min(chain["relanswer"].dropna())*1.2, max(chain["relanswer"].dropna())*1.2, 100)
         kde = gaussian_kde(chain["relanswer"].dropna())
         density = kde(y_grid)
         divider = make_axes_locatable(ax)
@@ -89,25 +100,39 @@ for ax, cond in zip(axs.flatten(), conds):
         hax.plot(density, y_grid, color=c, alpha=0.8, lw=1)
         hax.axis("off")
     ax.set_title(cond)
+    ax.text(5,-94, fr"${r'\overline{\xi_{\rm mean}}'}={np.mean(xis_mean):.2f}$; ${r'\overline{\xi_{\rm median}}'}={np.mean(xis_med):.2f}$")
     ax.axhline(0, color="k", alpha=0.3)
-    ax.set_ylim(-15,15)
+    ax.set_ylim(-100,100)
     if ax in axs[1,:]:
-        ax.set_xlabel("t")
+        ax.set_xlabel(f"rater $n$")
     else:
         ax.set_xticks([])
-axs[0,0].legend(fontsize=5)
-axs[0,0].set_ylabel("Deviation from y_true [yrs]", y=0)
-fig.suptitle("'how-old-is-X' questions")
+axs[0,0].legend(fontsize=4, ncol=3, frameon=False, handlelength=1,columnspacing=1, labelspacing=0, borderpad=0)
+axs[0,0].set_ylabel("Deviation from true rating [T]", y=0)
+# fig.suptitle("'how-old-is-X' questions")
+fig.suptitle("'Estimate movie rating on Rotten Tomatoes'")
+fig.tight_layout()
+plt.savefig("figs/rotten_tomatoes_ratings.png", dpi=600)
 
+xis_df = pd.DataFrame(xis, columns=["condition", "movie", "xi_mean", "xi_median"])
 # %%
-
+fig, ax = plt.subplots(1,1,figsize=(8/2.54, 8/2.54))
+xis_df_melted = xis_df.melt(id_vars=["condition", "movie"], value_name="xi", var_name="metric").replace({"xi_mean":"mean", "xi_median":"median"})
+sns.barplot(xis_df_melted, hue="condition", x="metric", y="xi", saturation=0.2)
+sns.stripplot(xis_df_melted, hue="condition", x="metric", y="xi", dodge=True, legend=False, size=2)
+plt.xlabel(None)
+plt.legend(title="", frameon=False, ncol=1)
+plt.ylabel(r"relative error $\xi$")
+plt.title("Collective performance\n'Estimate movie rating on Rotten Tomatoes'")
+fig.tight_layout()
 # %%
-selected_tasks = [10140+l for l in range(20)]
+selected_tasks = [10740+l for l in range(10)]
 results = []
 for task in selected_tasks:
     cond = "Control"
     chainControl = get_chain(task, cond)       
-    taskname = tasks.loc[tasks.task_id==task]["prompt"].str.replace("How old is ", "").str.replace("?", "").iloc[0]
+    # taskname = tasks.loc[tasks.task_id==task]["prompt"].str.replace("How old is ", "").str.replace("?", "").iloc[0]
+    taskname = tasks.loc[tasks.task_id==task]["path_to_media"].str.replace("prediction_movie_rating/", "").str.replace(".mp4", "").str.replace("_", " ").iloc[0]
     mu_p = np.mean(chainControl["relanswer"])
     sigma_p = np.std(chainControl["relanswer"], ddof=1)
 
